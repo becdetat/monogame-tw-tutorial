160fd8273006270cf9a61e3e54a4d55bfdbe6e38
Ben Scott <ben@belfryimages.com.au>
2014-05-25 20:57:55 +1000

    Add a test harness, move content projects into seperate solution

---
+++ src/Shooter.Tests/ThereShouldBeTests.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Shooter.Tests
+{
+    public class ThereShouldBeTests
+    {
+        [Fact]
+        public void BecauseTestsAreFun()
+        {
+            "So here are some tests".ShouldNotBeEmpty();
+        }
+    }
+}
